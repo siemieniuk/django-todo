9508f68d9bea81c4505bb4d043a2549bdb7d3e3b
Szymon Siemieniuk <szymonsiemieniuk01@gmail.com>
2021-07-30 00:00:24 +0200

    Improved date format and URL namespace

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('task/delete/<int:task_id>', views.delete_task_view, name='delete_task'),
     path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
-    path('task/new', views.create_task_view, name='newtask'),
+    path('task/new', views.create_task_view, name='new_task'),
     path('register', views.register_view, name='register'),
 ]
